045613bbf16352e98a99516907977e5773abd6d9
Tommy Groshong <tom@groshong.software>
2022-11-17 22:54:29 -0500

    Moar dropdown placeholders

--- src/dropdown.js
+++ src/dropdown.js
@@ -9,3 +9,39 @@ export class Dropdown extends HTMLElement {
     // write element functionality in here
   }
 }
+
+/**
+ * Triggering button that toggles the dropdown open or closed
+ */
+export class DropdownToggle extends HTMLElement {
+  constructor() {
+    // Always call super first in constructor
+    super();
+
+    // write element functionality in here
+  }
+}
+
+/**
+ * Container for dropdown list items
+ */
+export class DropdownMenu extends HTMLElement {
+  constructor() {
+    // Always call super first in constructor
+    super();
+
+    // write element functionality in here
+  }
+}
+
+/**
+ * Items in the dropdown list
+ */
+export class DropdownItem extends HTMLElement {
+  constructor() {
+    // Always call super first in constructor
+    super();
+
+    // write element functionality in here
+  }
+}
